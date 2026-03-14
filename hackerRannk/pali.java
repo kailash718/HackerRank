bf706256b07ad5c4d97c1821eb3e0dbb6c0cfcf0
kailash718 <104094357+kailash718@users.noreply.github.com>
2026-03-14 13:50:10 +0530

    Add palindrome check functionality
    Implement palindrome check in pali.java

---
+++ hackerRannk/pali.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        StringBuffer s=new StringBuffer(A);
+        if(A.equals(s.reverse().toString()))
+        {
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+        
+    }
+}
+
+
+
